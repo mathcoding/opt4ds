21fb431b09b6205b98fdc72c659c61663d25c80c
Stefano Gualandi <stefano.gualandi@gmail.com>
2026-03-23 13:57:35 +0100

    Update sudoku.py with class solution

--- scripts/sudoku.py
+++ scripts/sudoku.py
@@ -14,40 +14,59 @@ def SudokuSolver(Data):
     n = 3
     
     # Variables
+    x = {}
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                x[i, j, k] = model.addVar(vtype=GRB.BINARY, name=f"x_{i}_{j}_{k}")
     
     # Objective Function
     
     # 1. A single digit for each position
-    
+    for i in range(N):
+        for j in range(N):
+            model.addConstr(quicksum(x[i, j, k] for k in range(N)) == 1)
+
     # 2. Row constraints
+    for i in range(N):
+        for k in range(N):
+            model.addConstr(quicksum(x[i,j,k] for j in range(N)) == 1)
     
     # 3. Column constraints
+    for j in range(N):
+        for k in range(N):
+            model.addConstr(quicksum(x[i,j,k] for i in range(N)) == 1)
+    
                     
     # 4. Submatrix constraints
+    S = [0, 3, 6]
+    for i0 in S:
+        for j0 in S:
+            for k in range(N):
+                model.addConstr(quicksum(x[i0+i,j0+j,k] for i in range(n) for j in range(n))==1)
        
     # 5. Fix input data
+    for i in range(N):
+        for j in range(N):
+            if Data[i][j] > 0:
+                x[i,j,Data[i][j]-1].lb = 1
 
     # Solve model
     model.optimize()
 
-    # if model.Status == GRB.OPTIMAL:
-    #     S = np.zeros((N, N), dtype=int)
-    #     for i in range(N):
-    #         for j in range(N):
-    #             S[i, j] = sum(k*x[i, j, k].X for k in range(N))+1
+    if model.Status == GRB.OPTIMAL:
+        S = np.zeros((N, N), dtype=int)
+        for i in range(N):
+            for j in range(N):
+                S[i, j] = sum(k*x[i, j, k].X for k in range(N))+1
+        print(S)
 
-    #     for i in range(N):
-    #          for j in range(N):
-    #              for k in range(N):
-    #                  if x[i,j,k].X > 0:
-    #                      print(k+1, end="  ")
-    #          print()
-         sol = {}
-        # for i in range(N):
-        #      for j in range(N):
-        #          for k in range(N):
-        #                 if x[i,j,k].X > 0:
-        #                     sol[i,j,k] = k+1
+        sol = {}
+        for i in range(N):
+            for j in range(N):
+                for k in range(N):
+                    if x[i,j,k].X > 0.5:
+                        sol[i,j,k] = k+1
         return sol
     else:
         return None
@@ -63,11 +82,11 @@ def PlotSudoku(x, size=6):
 
     for i, j, k in x:
         if x[i,j,k] > 0:
-            if Data[i][j] == k:
-                plt.text(i, j, k, fontsize=4*size, color='red',
+            if Data[i][j]-1 == k:
+                plt.text(j, i, k+1, fontsize=4*size, color='red',
                      ha='center', va='center')
             else:                
-                plt.text(i, j, k, fontsize=4*size, color='darkblue',
+                plt.text(j, i, k+1, fontsize=4*size, color='darkblue',
                          ha='center', va='center')
              
     # Prettify output
@@ -100,7 +119,7 @@ if __name__ == "__main__":
             [0, 0, 3, 4, 5, 0, 0, 0, 0]]
 
     sol = SudokuSolver(Data)
-    print(sol)
+    
     # If x is "None" there is no feasible solution
     if sol is not None:
         PlotSudoku(sol)
